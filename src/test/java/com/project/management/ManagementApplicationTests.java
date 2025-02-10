71c8d464cc3f6f846a6be455cf4353a00193f2c4
Pedro <peuhenry2018@gmail.com>
2025-02-10 18:56:17 -0300

    Initial Commits ✨

---
+++ src/test/java/com/project/management/ManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.management;
+
+import org.junit.jupiter.api.Test;
+
+
+@com.project.management.SpringBootTest
+class ManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/com/project/management/SpringBootTest.java
@@ -0,0 +1,4 @@
+package com.project.management;
+
+public @interface SpringBootTest {
+}
