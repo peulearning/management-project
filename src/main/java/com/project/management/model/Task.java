0eda51961265c942c1bbd7c8bb37554527c66cb6
Pedro <peuhenry2018@gmail.com>
2025-02-11 17:54:26 -0300

    Task Entity Created ✅

--- src/main/java/com/project/management/model/Task.java
+++ src/main/java/com/project/management/model/Task.java
@@ -1,4 +1,31 @@
 package com.project.management.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Table(name = "tasks")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String title;
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private Priority priority;
+
+    @ManyToOne
+    @JoinColumn(name = "project_id")
+    private Project project;
 }
