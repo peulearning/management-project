bb2f8f797579799afb5d11c564c884a48ec3cddc
Pedro <peuhenry2018@gmail.com>
2025-02-11 18:02:20 -0300

    Task Service Implemented ⚠️

--- src/main/java/com/project/management/service/TaskService.java
+++ src/main/java/com/project/management/service/TaskService.java
@@ -1,4 +1,23 @@
 package com.project.management.service;
 
+import com.project.management.model.Task;
+import com.project.management.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class TaskService {
+
+    @Autowired
+    private TaskRepository repository;
+
+    public List<Task> findAll(){
+        return repository.findAll();
+    }
+
+    public Task save(Task task){
+        return repository.save(task);
+    }
 }
