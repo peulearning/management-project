da3b792f38550dffb8b22fca0b9347ca3efe1f37
Pedro <peuhenry2018@gmail.com>
2025-02-11 18:00:18 -0300

    Project Service Implemented ⚠️

--- src/main/java/com/project/management/service/ProjectService.java
+++ src/main/java/com/project/management/service/ProjectService.java
@@ -1,4 +1,23 @@
 package com.project.management.service;
 
+import com.project.management.model.Project;
+import com.project.management.repository.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProjectService {
+
+    @Autowired
+    private ProjectRepository repository;
+
+    public List<Project> findAll(){
+        return repository.findAll();
+    }
+
+    public Project save(Project project){
+        return repository.save(project);
+    }
 }
