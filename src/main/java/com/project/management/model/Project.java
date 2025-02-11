b209c49697c3b908ff3b951d14a3623e538b243c
Pedro <peuhenry2018@gmail.com>
2025-02-11 15:27:58 -0300

    Create Entity Project ✅

--- src/main/java/com/project/management/model/Project.java
+++ src/main/java/com/project/management/model/Project.java
@@ -1,4 +1,27 @@
 package com.project.management.model;
 
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Table(name = "projects")
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String title;
+    private String description;
+    private LocalDateTime dateBegin;
+
+    @OneToMany
+    private List<Task> tasks;
+
 }
