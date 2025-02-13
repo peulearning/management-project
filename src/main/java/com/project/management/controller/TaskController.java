ce28dbb0df929ba5496c880d076708bdc73bb280
Pedro <peuhenry2018@gmail.com>
2025-02-13 00:12:17 -0300

    Task Controller 🪄

--- src/main/java/com/project/management/controller/TaskController.java
+++ src/main/java/com/project/management/controller/TaskController.java
@@ -1,9 +1,30 @@
 package com.project.management.controller;
 
+import com.project.management.model.Task;
+import com.project.management.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
-@RequestMapping
+@RequestMapping("/tasks")
 public class TaskController {
+    @Autowired
+    private TaskService taskService;
+
+    @GetMapping
+    public String listTasks(Model model) {
+        List<Task> tasks = taskService.findAll();
+        model.addAttribute("tasks", tasks);
+        return "tasks";
+    }
+
+    @PostMapping
+    public String createTask(@ModelAttribute Task task) {
+        taskService.save(task);
+        return "redirect:/tasks";
+    }
 }
