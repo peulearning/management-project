bbae056660cc107efc87b851c1829efc5961d20d
Pedro <peuhenry2018@gmail.com>
2025-02-13 00:07:05 -0300

    Back-end ✅  / Front-end ⚠️ (Incomplete)

--- src/main/java/com/project/management/controller/TaskController.java
+++ src/main/java/com/project/management/controller/TaskController.java
@@ -1,4 +1,9 @@
 package com.project.management.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
 public class TaskController {
 }
